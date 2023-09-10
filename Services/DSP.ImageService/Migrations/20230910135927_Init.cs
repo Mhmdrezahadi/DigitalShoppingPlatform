6e044f45638f3432cfd865bace57076c1194d61f
mhmdreza_hadi <mhmdreza.hadi@gmail.com>
2023-09-10 17:30:10 +0330

    image service database

---
+++ Services/DSP.ImageService/Migrations/20230910135927_Init.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DSP.ImageService.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Full = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    Thumb = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    TimeCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
