9087c967b8c073cea8def71149c3e455ca861925
mhmdreza_hadi <mhmdreza.hadi@gmail.com>
2023-09-10 17:23:35 +0330

    create image methods in imageService

--- Services/DSP.ImageService/Program.cs
+++ Services/DSP.ImageService/Program.cs
@@ -1,4 +1,6 @@
+using DSP.ImageService.Data;
 using DSP.ImageService.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+builder.Services.AddDbContext<ImageServiceDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ImageServiceConnection"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<UploadService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
